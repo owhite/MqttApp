bda4159b0ccf6d7b1d90c956eaefac453806e4e3
“Atendra” <atendra@hastree.com>
2024-05-20 12:19:41 +0530

    Settings activity layout fixed, version code updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.owen.mqttapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
